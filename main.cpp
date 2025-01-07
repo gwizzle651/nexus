46e23bfc51bad85c1ccee59f5506ce32bb7f3701
gwizzle651 <gwizzle651@proton.me>
2025-01-07 08:07:20 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-//e
-int main() {
-    std::cout << "hello world";
-    return 0;
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);  // Initializes the application
+
+     app.setApplicationName("Nexus");
+    
+    QWidget window;  // Create a window
+    window.resize(250, 150);  // Set the size of the window
+    window.setWindowTitle("Basic Qt Application");  // Set the title of the window
+    window.show();  // Show the window
+
+    return app.exec();  // Starts the application event loop
 }
